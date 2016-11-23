77c0609a87d904e03dd6c08e9d5c83d24d8c8509
awunderground <awunderground@gmail.com>
2016-11-23 08:52:53 -0500

    Update .gitignore and add function to get_data.R that separates the messy Excel file into dfs for years without missing values

--- get_data.R
+++ get_data.R
@@ -1,7 +1,7 @@
 # Aaron Williams, Urban Institute Program on Retirement Policy
 
-# This script reads five decades of four measures of mean income for older 
-# Americans across from 
+# This script reads five decades worth of income projections for different 
+# demographic from the Microsoft Excel file
 # "X:\programs\run912\Run5SaveOpt4\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx".
 # The script also cleans the data and turns them into a long data frame 
 # formatted for ggplot2 
@@ -13,51 +13,41 @@ library(readxl)
 # Read unformatted data from Microsoft Excel
 distribution <- read_excel("X:/programs/Run912/run5SaveOpt4/BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "$income Distribution by Sour", skip = 4, col_names = FALSE)
 
-
-distribution2015 <- distribution %>%
-  select(c(X0, X1, X6:X13))
-
-names(distribution2015) <- c("group", "income.source", distribution2015[1, 3:ncol(distribution2015)])
-
-distribution2015 <- distribution2015 %>%
-  slice(-1) %>%
-  mutate(year = 2015)
-
-while (sum(is.na(distribution2015$group)) > 1) {
-
-  distribution2015 <- distribution2015 %>%
-    mutate(group = ifelse(is.na(group), lag(group), group))
+# Turn the data into untidy data frames for each year with no missing values
+cleanBPC <- function(column1, column2, column3, year) {
+  # Cleans raw data frame from read_excel and returns an untidy data frame
+  #
+  # Args: column names and year
+  #
+  # Returns: 
   
+  temp <- distribution %>%
+    select_(column1, column2, column3)
+  
+  names(temp) <- c("group", "income.source", temp[1, 3:ncol(temp)])
+  
+  temp <- temp %>%
+    slice(-1) %>%
+    mutate(year = year)
+  
+  while (sum(is.na(temp$group)) > 1) {
+    
+    temp <- temp %>%
+      mutate(group = ifelse(is.na(group), lag(group), group))
+    
   }
+  
+  temp <- temp %>%
+    filter(!is.na(P10) & P10 != "P10")
 
-distribution2015 <- distribution2015 %>%
-  filter(!is.na(P10) & P10 != "P10")
+  return(temp)
+  
+}
 
+distribution2015 <- cleanBPC(column1 = "X0", column2 = "X1", column3 = "X6:X13", year = 2015)
+distribution2025 <- cleanBPC(column1 = "X15", column2 = "X16", column3 = "X20:X27", year = 2025)
+distribution2035 <- cleanBPC(column1 = "X29", column2 = "X30", column3 = "X34:X41", year = 2035)
+distribution2045 <- cleanBPC(column1 = "X43", column2 = "X44", column3 = "X48:X55", year = 2045)
+distribution2055 <- cleanBPC(column1 = "X57", column2 = "X58", column3 = "X62:X69", year = 2055)
+distribution2065 <- cleanBPC(column1 = "X71", column2 = "X72", column3 = "X76:X83", year = 2065)
 
-
-# boom 
-
-
-
-
-
-
-
-
-
-
-
-distribution2025 <- distribution %>%
-  select(X15:X27)
-
-distribution2035 <- distribution %>%
-  select()
-
-distribution2045 <- distribution %>%
-  select()
-
-distribution2055 <- distribution %>%
-  select()
-
-distribution2065 <- distribution %>%
-  select()
