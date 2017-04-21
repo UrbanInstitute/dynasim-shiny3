a41c160a4c2626f5d9e0dc9eb0ed9e8c11de31c3
awunderground <awunderground@gmail.com>
2017-04-21 17:16:24 -0400

    Move all descriptions to .csv files and drop long if-then chains.

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(extrafont)
 library(grid)
 library(RColorBrewer)
 library(scales)
+library(ggiraph)
 
 options(scipen = 999)
 
@@ -17,6 +18,10 @@ source('urban_institute_themes/urban_theme_windows.R')
 
 # Load Data
 distribution <- read_csv("data/distributions.csv")
+scale_text <- read_csv("text/scale.csv")
+baseline_text <- read_csv("text/baseline.csv")
+income_tax_premium_text <- read_csv("text/income_tax_premium.csv")
+option_text <- read_csv("text/option.csv")
 
 # Gather the data
 distribution <- distribution %>%
@@ -43,30 +48,35 @@ distribution <- distribution %>%
                                                 "Some College",
                                                 "College Graduates"),
                                       labels = c("All Individuals",
-                                                 "Females",
-                                                 "Males",
-                                                 "African-Americans",
-                                                 "Hispanics",
-                                                 "White, Non-Hispanics",
+                                                 "Female",
+                                                 "Male",
+                                                 "Black",
+                                                 "Hispanic",
+                                                 "White, Non-Hispanic",
                                                  "Bottom Quintile",
                                                  "2nd Quintile",
                                                  "3rd Quintile",
                                                  "4th Quintile",
                                                  "Top Quintile",
-                                                 "Never Married Individuals",
-                                                 "Divorced Individuals",
-                                                 "Married Individuals",
-                                                 "Widowed Individuals",
-                                                 "High School Dropouts",
-                                                 "High School Graduates",
+                                                 "Never Married",
+                                                 "Divorced",
+                                                 "Married",
+                                                 "Widowed",
+                                                 "HS Dropout",
+                                                 "HS Graduate",
                                                  "Some College",
-                                                 "College Graduates"))) %>%
+                                                 "College Graduate"))) %>%
   gather(`Annuitized Financial Income`:`State Income Tax`, key = income.tax.premium, value = value)
 
 ##
 ## SHINY
 ##
+
+latoCSS <- "http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic"
+
 ui <- fluidPage(
+  
+  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = latoCSS)),
   
   theme = "shiny.css",
   
@@ -91,7 +101,7 @@ ui <- fluidPage(
   fluidRow(
     column(6,
            
-           plotOutput("chart")
+           ggiraphOutput("chart", width = "100%", height = "400px")
            
            )
   ),
@@ -243,7 +253,7 @@ server <- function(input, output) {
   
   options(shiny.sanitize.errors = FALSE)
   
-  output$chart <- renderPlot({  
+  output$chart <- renderggiraph({  
   
     title <- if (input$income.tax.premium == "Annuitized Financial Income") {"Annuitized Financial Income ($2015)"} else
              if (input$income.tax.premium == "DB Pension Income") {"Defined Benefit Pension Income ($2015)"} else
@@ -307,7 +317,9 @@ server <- function(input, output) {
     
     graphr <- function(origin, line.placement, line.color){
     
-      distribution %>%
+      distribution$tooltip <- distribution$value
+      
+      gg_bar <- distribution %>%
         filter(option == input$option) %>%
         filter(group == input$group) %>%  
         filter(year == input$year) %>%
@@ -317,18 +329,22 @@ server <- function(input, output) {
         filter(income.tax.premium == input$income.tax.premium) %>%
         filter(percentile != "Percent with Income Source") %>%
         ggplot() +
-        geom_bar(aes(x = percentile, y = value, fill = subgroup), position = "dodge", stat = "identity") +
-        scale_y_continuous(limits = c(y.min, as.numeric(y.max)), labels = scales::dollar) +
-        labs(title = title,
-             subtitle = subtitle,
-             caption = "DYNASIM3") +
-        xlab("Mean and Percentiles") +
-        ylab(NULL) +
-        expand_limits(y = origin) +
-        geom_hline(size = 0.5, aes(yintercept = line.placement), color = line.color) +
-        theme(axis.ticks.length = unit(0, "points"),
-              axis.line = element_blank())
+          geom_bar_interactive(aes(x = percentile, y = value, fill = subgroup, tooltip = tooltip), position = "dodge", stat = "identity") +
+          scale_y_continuous(limits = c(y.min, as.numeric(y.max)), labels = scales::dollar) +
+          labs(title = title,
+               subtitle = subtitle,
+               caption = "DYNASIM3") +
+          xlab("Mean and Percentiles") +
+          ylab(NULL) +
+          expand_limits(y = origin) +
+          geom_hline(size = 0.5, aes(yintercept = line.placement), color = line.color) +
+          theme_urban() +
+          theme(axis.ticks.length = unit(0, "points"),
+                axis.line = element_blank())
     
+      ggiraph(code = {print(gg_bar)}, width = 1, height = 5.418)
+      
+      
     }
       
     if (input$comparison == "level") {
@@ -341,118 +357,42 @@ server <- function(input, output) {
   })  
   
     output$text1 <- renderText({
-      
-      if (input$option == "BPC Package") {"<p><h4>BPC Package</h4></p><p>Annual PIA, limit spousal benefits, replace the WEP and GPO with a proportional reduction in OASI benefits based on covered earnings, enhance survivor benefits, increase the progressivity of the benefit formula, increase Social Security tax max to $195,000, payroll tax to 13.4% and FRA to 69, switch to C-CPI-U for COLAs, end 'claim-and-suspend' games, create a basic minimum benefit for all individuals above the FRA eligible for Social Security, and tax 100 percent of Social Security benefits for beneficiaries with annual incomes above $250,000.</p>"}
-      
-      else if (input$option == "Annual PIA") {"<p><h4>Annual PIA</h4></p><p>Eliminates the preferential treatment of workers with short careers by applying Social Security’s progressive benefit formula to the 40 highest years of wage-indexed earnings divided by 37 rather than applying the formula to total wage-indexed earnings received in the top 35 years. It also makes the benefit formula more progressive. This begins with OASI claimants who attain age 62 in 2022.</p>"}
-      
-      else if (input$option == "Increase Benefits Taxation") {"<p><h4>Increase Benefits Taxation</h4></p><p>Increases the taxation of Social Security benefits.</p>"}
-      
-      else if (input$option == "Cap Spouse Benefits") {"<p><h4>Cap Spouse Benefits</h4></p><p>Caps the spouse benefit at $1,121.68 in 2016 beginning for claimants who turn 60 in 2020 and beyond. Indexes the cap annually by chained CPI-U.</p>"}
-      
-      else if (input$option == "75% Survivor Benefit") {"<p><h4>75% Survivor Benefit</h4></p><p>Increases joint-and-survivors benefits to 75 percent of combined benefits for the couple, from 50 percent of combined benefits, for claimants who turn 62 in 2022 and beyond.</p>"}
-      
-      else if (input$option == "90% Tax Max") {"<p><h4>90% Tax Max</h4></p><p>Raises the cap on annual earnings subject to the Social Security payroll tax and that enter the benefits calculation to cover 90 percent of payroll. This increase is phased in over 10 years, beginning in 2016.</p>"}
-      
-      else if (input$option == "90% Tax Max and 13.4% Payroll Tax") {"<p><h4>90% Tax Max and 13.4% Payroll Tax</h4></p><p>Raises the cap on annual earnings subject to the Social Security payroll tax and that enter the benefits calculation to cover 90 percent of payroll. This increase is phased in over 10 years, beginning in 2016. Also, increase the payroll tax to 13.4% over t10 years beginning in 2016.</p>"}
-      
-      else if (input$option == "Full Chained-CPI COLA") {"<p><h4>Full Chained-CPI COLA</h4></p><p>Ties beneficiaries' annual cost-of-living-adjustment (COLA) to the change in the chained consumer price index (C-CPI-U), which grows more slowly than the standard CPI-U now used to compute COLAs. (Only those NRA or older)</p>"}
-      
-      else if (input$option == "Partial Chained-CPI COLA") {"<p><h4>Partial Chained-CPI COLA</h4></p><p>Ties beneficiaries' annual cost-of-living-adjustment (COLA) to the change in the chained consumer price index (C-CPI-U), which grows more slowly than the standard CPI-U now used to compute COLAs. (All beneficiaries including those under the NRA)</p>"}
-      
-      else if (input$option == "Increase FRA") {"<p><h4>Increase FRA</h4></p><p>Indefinitely raises Social Security's FRA (now set at 67 beginning in 2022) and the age for receiving delayed retirement credits by one month every two years, beginning in 2024.</p>"}
-      
-      else if (input$option == "Increase FRA and EEA") {"<p><h4>Increase EEA & FRA</h4></p><p>Raises Social Security's early eligibility age (EEA), which is now set at 62, and indefinitely raises Social Security's FRA (now set at 67 beginning in 2022) and the age for receiving delayed retirement credits by one month every two years, beginning in 2024.</p>"}
-      
-      else if (input$option == "$150,000 Tax Max") {"<p><h4>$150,000 Tax Max</h4></p><p>Increase the tax cap to $150,000 between 2016 and 2018 and then increase the tax cap by wage growth plus 0.5 percentage points thereafter.</p>"}
-      
-      else if (input$option == "$180,000 Tax Max") {"<p><h4>$180,000 Tax Max</h4></p><p>Increase the tax cap to $180,000 between 2016 and 2018 and then increase the tax cap by wage growth plus 0.5 percentage points thereafter.</p>"}
-      
-      else if (input$option == "Eliminate the Tax Max") {"<p><h4>Eliminate the Tax Max</h4></p><p>Eliminates the cap on annual earnings subject to the Social Security payroll tax and that enter the benefits calculation.</p>"}
-      
-      else if (input$option == "13.4% Payroll Tax") {"<p><h4>13.4% Payroll Tax</h4></p><p>Increase the payroll tax rate to 13.4% over 10 years beginning in 2016.</p>"}
-      
-      else if (input$option == "14.4% Payroll Tax") {"<p><h4>14.4% Payroll Tax</h4></p><p>Increase the payroll tax rate to 14.4% over 10 years beginning in 2016.</p>"}
-      
-      else if (input$option == "15.4% Payroll Tax") {"<p><h4>15.4% Payroll Tax</h4></p><p>Increase the payroll tax rate to 15.4% over 10 years beginning in 2016.</p>"}
-      
-      else if (input$option == "Payable Law") {"<p><h4>Current Law Payable</h4></p><p>Assumes that current public policies, business practices, and individual behaviors continue, and that Social Security benefits are paid as promised, even after the trust fund runs out. </p>"}
-      
-      else if (input$option == "Scheduled Law") {"<p><h4>Current Law Scheduled</h4></p><p>Assumes that current public policies, business practices, and individual behaviors continue, and that Social Security benefits are paid as promised, even after the trust fund runs out.</p>"}
-      
-      })
     
+      as.character(
+        option_text %>%
+          filter(option == input$option) %>%
+          select(text)
+      )
+    
+    })
     
     output$text2 <- renderText({
       
-      if (input$income.tax.premium == "Annuitized Financial Income") {"<p><h4>Annuitized Financial Income</h4></p><p></p>"}
+      as.character(
+        income_tax_premium_text %>%
+          filter(income_tax_premium == input$income.tax.premium) %>%
+          select(text)
+      )
       
-      else if (input$income.tax.premium == "DB Pension Income") {"<p><h4>DB Pension Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Earned Income") {"<p><h4>Earned Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Federal Income Tax") {"<p><h4>Federal Income Tax</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "HI Tax") {"<p><h4>HI Tax</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Imputed Rental Income") {"<p><h4>Imputed Rental Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Means and Non-Means Tested Benefits") {"<p><h4>Means and Non-Means Tested Benefits</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Medicare Part B Premium") {"<p><h4>Medicare Part B Premium</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Medicare Surtax") {"<p><h4>Medicare Surtax</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Net Annuity Income") {"<p><h4>Net Annuity Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Net Cash Income") {"<p><h4>Net Cash Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "OASDI Tax") {"<p><h4>OASDI Tax</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Other Family Member Income") {"<p><h4>Other Family Member Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Own Benefit") {"<p><h4>Own Benefit</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Own Earnings") {"<p><h4>Own Earnings</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Annuity Income") {"<p><h4>Annuity Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Cash Income") {"<p><h4>Cash Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Dividend Income") {"<p><h4>Dividend Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Interest Income") {"<p><h4>Interest Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "IRA Withdrawal") {"<p><h4>IRA Withdrawal</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Rental Income") {"<p><h4>Rental Income</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Social Security Benefits") {"<p><h4>Social Security Benefits</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Spouse Benefit") {"<p><h4>Spouse Benefit</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "Spouse Earnings") {"<p><h4>Spouse Earnings</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "SSI") {"<p><h4>SSI</h4></p><p></p>"}
-      
-      else if (input$income.tax.premium == "State Income Tax") {"<p><h4>State Income Tax</h4></p><p></p>"}
-      
-      })
+    })
     
     output$text3 <- renderText({
       
-      if (input$scale == "per capita") {"<p><h4>Per Capita</h4></p><p></p>"}
-      
-      else if (input$scale == "equivalent") {"<p><h4>Equivalent</h4></p><p></p>"}
-      
-      })
-    
+      as.character(
+        scale_text %>%
+          filter(scale == input$scale) %>%
+          select(text)
+      )
+  
+    })
     
     output$text4 <- renderText({
       
-      if (input$baseline == "Payable Law") {"<p><h4>Current Law Payable</h4></p><p>Assumes that current public policies, business practices, and individual behaviors continue, but reduces Social Security benefits by a uniform amount after the trust fund runs out so that all benefits in each year can be paid out of revenues from that year.</p>"}
-      
-      else if (input$baseline == "Scheduled Law") {"<p><h4>Current Law Scheduled</h4></p><p>Assumes that current public policies, business practices, and individual behaviors continue, and that Social Security benefits are paid as promised, even after the trust fund runs out.</p>"}
+      as.character(
+        baseline_text %>%
+          filter(baseline == input$baseline) %>%
+          select(text)
+      )
       
     })
     
@@ -472,8 +412,6 @@ server <- function(input, output) {
       HTML(paste("<div class='income-percent'>", as.character(round(percent * 100, 1)), "%", "</div>","<div class='income-text'>", "have", "<b>", input$income.tax.premium, "</b>", "</div>"))
       
     })
-    
-
     
 }
     
